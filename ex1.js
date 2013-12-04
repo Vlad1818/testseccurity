5bee165f9d5f7b3814322100976c83f90f274024
Vlad Shumlin <vsh1818@gmail.com>
2013-12-04 17:12:36 +0200

    dumb

--- ex1.js
+++ ex1.js
@@ -5,65 +5,63 @@ var app = express();
 function HandleRequest(req, res){
 var start = new Date().getTime();
  res.writeHead(200, {'Content-Type': 'text/html'});
-res.write('<html>
-<head>
-<script type="text/javascript">
-	i=0;
-	sites = [\'http://google.com\', \'http://facebook.com\', \'http://youtube.com\', \'http://yahoo.com\', 
-	\'http://wikipedia.org\', \'http://linkedin.com\', \'http://live.com\', \'http://twitter.com\', \'http://amazon.com\'];
-	firstLoad=[0,0,0,0,0,0,0,0,0,0];
-	cachedTimes=[0,0,0,0,0,0,0,0,0,0];
-	viewRatios=[1.6,1.4,1.35,1.1,3,1.4,1.4,1.5,2,2];
-	beforeload = (new Date()).getTime();
-	loadCount = 0;
-	const TIMES_TO_LOAD=4;
-	
-	function pageloadingtime()
-	{
-		var afterload = (new Date()).getTime();
-		var seconds = (afterload-beforeload)/1000;
-		if(loadCount == 0){
-			firstLoad[i]=seconds;}
-		else{
-			cachedTimes[i]+=seconds;}
-		document.getElementById("loadingtime").innerHTML = document.getElementById("loadingtime").innerHTML+"<font color=\'red\'>"+sites[i]+" Load "+(loadCount+1)+" took " + seconds + " seconds</font></br>";
-		loadCount++;
-		if(loadCount==TIMES_TO_LOAD){
-			loadCount=0;
-			i++;
-		}
-		var f = document.getElementById(\'iframe1\');
-		if(i<sites.length){
-			beforeload = (new Date()).getTime();
-			f.src = sites[i];
-		}
-		else{
-			printFinalData();
-		}
-	}
-	
-	function printFinalData()
-	{
-		for (var j=0;j<sites.length;j++)
-		{ 
-			document.getElementById("loadingtime").innerHTML = document.getElementById("loadingtime").innerHTML+"<font color=\'blue\'>"+sites[j]+" first load took "+firstLoad[j]+" and cached load takes " + (cachedTimes[j]/(TIMES_TO_LOAD-1)) + " on average</font></br>";
-		}
-		for (var j=0;j<sites.length;j++)
-		{ 
-			if(((cachedTimes[j]/(TIMES_TO_LOAD-1))*viewRatios[j])>=firstLoad[j])
-				document.getElementById("loadingtime").innerHTML = document.getElementById("loadingtime").innerHTML+"<font color=\'green\'>You have visited"+sites[j]+"!</font></br>";
-			else
-				document.getElementById("loadingtime").innerHTML = document.getElementById("loadingtime").innerHTML+"<font color=\'purple\'>You did not visited"+sites[j]+"!</font></br>";
-		}
-	}
-</script>
-</head>
-<body>
-<div id="loadingtime"></div>
-<br>
-	<iframe id="iframe1" src="http://google.com" width="0" height="0"  onload="pageloadingtime();"></iframe>
-</body>
-<html>');
+res.write('<html>');
+res.write('<head>');
+res.write('<script type="text/javascript">');
+res.write('	i=0;');
+res.write('	sites = [\'http://google.com\', \'http://facebook.com\', \'http://youtube.com\', \'http://yahoo.com\', ');
+res.write('	\'http://wikipedia.org\', \'http://linkedin.com\', \'http://live.com\', \'http://twitter.com\', \'http://amazon.com\'];');
+res.write('	firstLoad=[0,0,0,0,0,0,0,0,0,0];');
+res.write('	cachedTimes=[0,0,0,0,0,0,0,0,0,0];');
+res.write('	viewRatios=[1.6,1.4,1.35,1.1,3,1.4,1.4,1.5,2,2];');
+res.write('	beforeload = (new Date()).getTime();');
+res.write('	loadCount = 0;');
+res.write('	const TIMES_TO_LOAD=4;');
+res.write('	function pageloadingtime()');
+res.write('	{');
+res.write('		var afterload = (new Date()).getTime();');
+res.write('		var seconds = (afterload-beforeload)/1000;');
+res.write('		if(loadCount == 0){');
+res.write('			firstLoad[i]=seconds;}');
+res.write('		else{');
+res.write('			cachedTimes[i]+=seconds;}');
+res.write('		document.getElementById("loadingtime").innerHTML = document.getElementById("loadingtime").innerHTML+"<font color=\'red\'>"+sites[i]+" Load "+(loadCount+1)+" took " + seconds + " seconds</font></br>";');
+res.write('		loadCount++;');
+res.write('		if(loadCount==TIMES_TO_LOAD){');
+res.write('			loadCount=0;');
+res.write('			i++;');
+res.write('		}');
+res.write('		var f = document.getElementById(\'iframe1\');');
+res.write('		if(i<sites.length){');
+res.write('			beforeload = (new Date()).getTime();');
+res.write('			f.src = sites[i];');
+res.write('		}');
+res.write('		else{');
+res.write('			printFinalData();');
+res.write('		}');
+res.write('	}	');
+res.write('	function printFinalData()');
+res.write('	{');
+res.write('		for (var j=0;j<sites.length;j++)');
+res.write('		{ ');
+res.write('			document.getElementById("loadingtime").innerHTML = document.getElementById("loadingtime").innerHTML+"<font color=\'blue\'>"+sites[j]+" first load took "+firstLoad[j]+" and cached load takes " + (cachedTimes[j]/(TIMES_TO_LOAD-1)) + " on average</font></br>";');
+res.write('		}');
+res.write('		for (var j=0;j<sites.length;j++)');
+res.write('		{ ');
+res.write('			if(((cachedTimes[j]/(TIMES_TO_LOAD-1))*viewRatios[j])>=firstLoad[j])');
+res.write('				document.getElementById("loadingtime").innerHTML = document.getElementById("loadingtime").innerHTML+"<font color=\'green\'>You have visited"+sites[j]+"!</font></br>";');
+res.write('			else');
+res.write('				document.getElementById("loadingtime").innerHTML = document.getElementById("loadingtime").innerHTML+"<font color=\'purple\'>You did not visited"+sites[j]+"!</font></br>";');
+res.write('		}');
+res.write('	}');
+res.write('</script>');
+res.write('</head>');
+res.write('<body>');
+res.write('<div id="loadingtime"></div>');
+res.write('<br>');
+res.write('	<iframe id="iframe1" src="http://google.com" width="0" height="0"  onload="pageloadingtime();"></iframe>');
+res.write('</body>');
+res.write('<html>');
 res.end();
 var end = new Date().getTime();
 var time = end - start;
